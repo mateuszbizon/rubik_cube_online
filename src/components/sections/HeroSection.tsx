3e3498922ae2918ce8185b2ef4a89f2a2abc1b20
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-15 13:56:20 +0100

    added ui button

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -1,18 +1,27 @@
 import React from 'react'
 import Container from '../Container'
+import { Button } from '../ui/button'
 
 function HeroSection() {
   return (
     <section className="h-screen header-background">
         <Container>
             <div className='h-full grid lg:grid-cols-2'>
-                <div className='h-full flex flex-col justify-center gap-8'>
-                    <h1 className='heading1 text-dark text-center lg:text-left'>
+                <div className='h-full flex flex-col justify-center gap-8 text-center lg:text-left'>
+                    <h1 className='heading1 text-dark'>
                         Nauka kostki Rubika online
                     </h1>
                     <p className='text-2xl font-medium'>
                         Naucz się układać legendarną łamigłówkę logiczną jaką jest kostka Rubika w przyjazny i zrozumiały sposób.
                     </p>
+                    <div className='flex gap-5 justify-center lg:justify-start'>
+                        <Button size={"lg"}>
+                            Rozpocznij
+                        </Button>
+                        <Button variant={"secondary-black"} size={"lg"}>
+                            Opinie
+                        </Button>
+                    </div>
                 </div>
             </div>
         </Container>
