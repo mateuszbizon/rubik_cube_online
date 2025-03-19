3e8ff40dca5c0ecf14c28ad170f151241c439e32
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-19 11:24:49 +0100

    added animations for hero text

--- src/components/sections/HeroSection.tsx
+++ src/components/sections/HeroSection.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import Container from '../Container'
 import { Button } from '../ui/button'
 import Link from 'next/link'
+import AnimatedContent from '../animations/AnimatedContent/AnimatedContent'
+import FadeContent from '../animations/FadeContent/FadeContent'
 
 function HeroSection() {
   return (
@@ -9,24 +11,30 @@ function HeroSection() {
         <Container>
             <div className='h-full grid lg:grid-cols-2'>
                 <div className='h-full flex flex-col justify-center gap-8 text-center lg:text-left'>
-                    <h1 className='heading1 text-dark'>
-                        Nauka kostki Rubika online
-                    </h1>
-                    <p className='text-2xl font-medium'>
-                        Naucz się układać legendarną łamigłówkę logiczną jaką jest kostka Rubika w przyjazny i zrozumiały sposób.
-                    </p>
-                    <div className='flex gap-5 justify-center lg:justify-start flex-wrap'>
-                        <Button size={"lg"} asChild>
-                            <Link href={"#lesson"}>
-                                Rozpocznij
-                            </Link>
-                        </Button>
-                        <Button variant={"secondary-black"} size={"lg"} asChild>
-                            <Link href={"#reviews"}>
-                                Opinie
-                            </Link>
-                        </Button>
-                    </div>
+                    <AnimatedContent reverse={true} direction="horizontal">
+                        <h1 className='heading1 text-dark'>
+                            Nauka kostki Rubika online
+                        </h1>
+                    </AnimatedContent>
+                    <AnimatedContent delay={500}>
+                        <p className='text-2xl font-medium'>
+                            Naucz się układać legendarną łamigłówkę logiczną jaką jest kostka Rubika w przyjazny i zrozumiały sposób.
+                        </p>
+                    </AnimatedContent>
+                    <FadeContent blur={true} delay={1500}>
+                        <div className='flex gap-5 justify-center lg:justify-start flex-wrap'>
+                            <Button size={"lg"} asChild>
+                                <Link href={"#lesson"}>
+                                    Rozpocznij
+                                </Link>
+                            </Button>
+                            <Button variant={"secondary-black"} size={"lg"} asChild>
+                                <Link href={"#reviews"}>
+                                    Opinie
+                                </Link>
+                            </Button>
+                        </div>
+                    </FadeContent>
                 </div>
             </div>
         </Container>
