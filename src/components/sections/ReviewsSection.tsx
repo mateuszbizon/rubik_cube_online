4b6cb7b55016205a762c80987ae68eeac05d7ee9
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-20 13:45:18 +0100

    added link to all reviews

--- src/components/sections/ReviewsSection.tsx
+++ src/components/sections/ReviewsSection.tsx
@@ -6,6 +6,8 @@ import { Carousel, CarouselContent, CarouselNext, CarouselPrevious } from '../ui
 import Autoplay from "embla-carousel-autoplay"
 import { REVIEWS_LIST } from '@/constants/reviewsList'
 import ReviewCard from '../cards/ReviewCard'
+import { Button } from '../ui/button'
+import Link from 'next/link'
 
 function ReviewsSection() {
     const plugin = React.useRef(
@@ -25,7 +27,7 @@ function ReviewsSection() {
                 plugins={[plugin.current]}
                 onMouseEnter={plugin.current.stop}
                 onMouseLeave={plugin.current.reset}
-                className='lg:px-10'
+                className='lg:px-10 mb-10'
             >
                 <CarouselContent>
                     {REVIEWS_LIST.map(item => (
@@ -35,6 +37,13 @@ function ReviewsSection() {
                 <CarouselPrevious variant={"icon"} className='hidden lg:inline-flex' />
                 <CarouselNext variant={"icon"} className='hidden lg:inline-flex' />
             </Carousel>
+            <div className='flex justify-center'>
+                <Button size={"lg"} asChild>
+                    <Link href={"https://www.superprof.pl/speedcuber-letnim-doswiadczeniem-ukladajacy-kostke-rubika-srednio-ponizej-sekund-udzielam-prywatnych-lekcji-jak.html"} target='_blank'>
+                        Wszystkie opinie
+                    </Link>
+                </Button>
+            </div>
         </Container>
     </section>
   )
