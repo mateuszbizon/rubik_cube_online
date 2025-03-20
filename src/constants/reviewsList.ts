39c88ebab8bcebd5633433876a2462c655e85f10
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-20 19:13:30 +0100

    changed reviews list items structure

--- src/constants/reviewsList.ts
+++ src/constants/reviewsList.ts
@@ -1,9 +1,15 @@
 export type Review = {
-    name: string
-    content: string
-}
+	name: string;
+	content: string;
+};
 
 export const REVIEWS_LIST: Review[] = [
-    { name: "Magdalena", content: "Idealnie! Syn zadowolony;) Mateusz punktualny, zaangażowany. Otrzymaliśmy nawet dodatkowe materiały, dziękuję." },
-    { name: "Nastassia", content: "Idealnie! Pan Mateusz to prawdziwy mistrz w swoim fachu! Powoli i cierpliwie tłumaczy mojemu 7-letniemu synowi tajniki układania kostki. Zdarzają się momenty, kiedy syn się poddaje, ale pan Mateusz wyjaśnia mu jeszcze raz i jeszcze raz, aż w końcu zrozumie :) Siedząc obok syna, sama prawie nauczyłam się tej niezwykłej sztuki :) Bardzo dziękujemy, panie Mateuszu!" },
-]
+	{
+		name: "Magdalena",
+		content: "Idealnie! Syn zadowolony;) Mateusz punktualny, zaangażowany. Otrzymaliśmy nawet dodatkowe materiały, dziękuję.",
+	},
+	{
+		name: "Nastassia",
+		content: "Idealnie! Pan Mateusz to prawdziwy mistrz w swoim fachu! Powoli i cierpliwie tłumaczy mojemu 7-letniemu synowi tajniki układania kostki. Zdarzają się momenty, kiedy syn się poddaje, ale pan Mateusz wyjaśnia mu jeszcze raz i jeszcze raz, aż w końcu zrozumie :) Siedząc obok syna, sama prawie nauczyłam się tej niezwykłej sztuki :) Bardzo dziękujemy, panie Mateuszu!",
+	},
+];
