9203ad1c051fc069b36592260c8c29e557fbb82c
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-03-20 19:12:41 +0100

    deleted test reviews

--- src/constants/reviewsList.ts
+++ src/constants/reviewsList.ts
@@ -6,7 +6,4 @@ export type Review = {
 export const REVIEWS_LIST: Review[] = [
     { name: "Magdalena", content: "Idealnie! Syn zadowolony;) Mateusz punktualny, zaangażowany. Otrzymaliśmy nawet dodatkowe materiały, dziękuję." },
     { name: "Nastassia", content: "Idealnie! Pan Mateusz to prawdziwy mistrz w swoim fachu! Powoli i cierpliwie tłumaczy mojemu 7-letniemu synowi tajniki układania kostki. Zdarzają się momenty, kiedy syn się poddaje, ale pan Mateusz wyjaśnia mu jeszcze raz i jeszcze raz, aż w końcu zrozumie :) Siedząc obok syna, sama prawie nauczyłam się tej niezwykłej sztuki :) Bardzo dziękujemy, panie Mateuszu!" },
-    { name: "Osoba3", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, asperiores. Ipsum unde, adipisci sint architecto, eligendi commodi magnam earum voluptas officiis id possimus repudiandae itaque, vitae maxime et necessitatibus est?" },
-    { name: "Osoba4", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, asperiores. Ipsum unde, adipisci sint architecto, eligendi commodi magnam earum voluptas officiis id possimus repudiandae itaque, vitae maxime et necessitatibus est?" },
-    { name: "Osoba5", content: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Inventore, asperiores. Ipsum unde, adipisci sint architecto, eligendi commodi magnam earum voluptas officiis id possimus repudiandae itaque, vitae maxime et necessitatibus est?" },
 ]
