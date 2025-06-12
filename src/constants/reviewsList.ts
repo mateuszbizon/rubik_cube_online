6cac5e5b7418a17afe12e601a43861dcf5281e66
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-12 13:01:35 +0200

    added reviews

--- src/constants/reviewsList.ts
+++ src/constants/reviewsList.ts
@@ -12,4 +12,12 @@ export const REVIEWS_LIST: Review[] = [
 		name: "Nastassia",
 		content: "Idealnie! Pan Mateusz to prawdziwy mistrz w swoim fachu! Powoli i cierpliwie tłumaczy mojemu 7-letniemu synowi tajniki układania kostki. Zdarzają się momenty, kiedy syn się poddaje, ale pan Mateusz wyjaśnia mu jeszcze raz i jeszcze raz, aż w końcu zrozumie :) Siedząc obok syna, sama prawie nauczyłam się tej niezwykłej sztuki :) Bardzo dziękujemy, panie Mateuszu!",
 	},
+    {
+        name: "Bartosz",
+        content: "Super, Nauczył Syna układać kostkę 3x3"
+    },
+    {
+        name: "Justyna",
+        content: "Syn lubi lekcje z panem Mateuszem :) Polecam :)"
+    },
 ];
