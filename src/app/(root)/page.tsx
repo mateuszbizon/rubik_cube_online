ad7de67aefd26ae3882f36e06a9eaa7b1f82b889
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-12 17:21:16 +0200

    added quote section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -3,6 +3,7 @@ import ContactSection from "@/components/sections/ContactSection";
 import HeroSection from "@/components/sections/HeroSection";
 import LessonSection from "@/components/sections/LessonSection";
 import OfferSection from "@/components/sections/OfferSection";
+import QuoteSection from "@/components/sections/QuoteSection";
 import ReviewsSection from "@/components/sections/ReviewsSection";
 
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
         <AboutSection />
         <LessonSection />
         <OfferSection />
+        <QuoteSection />
         <ReviewsSection />
         <ContactSection />
     </>
---
+++ src/components/sections/QuoteSection.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Container from '../Container'
+import { Quote } from 'lucide-react'
+
+function QuoteSection() {
+  return (
+    <section className='relative text-light py-[100px] bg-[url(/quote_bg_small.jpg)] bg-center bg-no-repeat bg-cover bg-fixed z-0'>
+        <div className='absolute inset-0 bg-black/60 -z-10'></div>
+        <Quote className='absolute top-10 right-10 md:right-20 size-9 rotate-12' />
+        <Quote className='absolute bottom-10 left-10 md:left-20 size-9 -rotate-30' />
+        <Container>
+            <h2 className='heading2 text-center leading-15 z-10'><em>Nie ważne ile masz lat, czy umiesz układać kostkę lub skąd jesteś. Razem określimy Twój poziom oraz cele, aby poprawić swoje umiejętności.</em></h2>
+        </Container>
+    </section>
+  )
+}
+
+export default QuoteSection
