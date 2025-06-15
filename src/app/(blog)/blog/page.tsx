0ff9afeac6bd585271a3e79c39bd3dc82e12b8b4
Mateusz Bizoń <92033267+mateuszbizon@users.noreply.github.com>
2025-06-15 22:20:44 +0200

    added all blogs component

--- src/app/(blog)/blog/page.tsx
+++ src/app/(blog)/blog/page.tsx
@@ -1,22 +1,17 @@
-import BlogCard from '@/components/cards/BlogCard'
+import AllBlogs from '@/components/blog/AllBlogs'
 import Container from '@/components/Container'
-import { client } from '@/sanity/lib/client'
-import { GET_ALL_BLOGS_QUERY } from '@/sanity/lib/queries'
-import React from 'react'
+import GridLoading from '@/components/ui/grid-loading'
+import React, { Suspense } from 'react'
 
-async function BlogPage() {
-    const blogs = await client.fetch(GET_ALL_BLOGS_QUERY)
-
+function BlogPage() {
   return (
     <div className='py-10'>
         <Container>
             <h1 className='heading1 mb-10'>Blogi</h1>
             <p className='bigger-text mb-7'>Poniżej znajdziesz listę wszystkich blogów utworzonych na tej stronie</p>
-            <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10'>
-                {blogs.map(item => (
-                    <BlogCard key={item._id} blog={item} />
-                ))}
-            </div>
+            <Suspense fallback={<GridLoading />}>
+                <AllBlogs />
+            </Suspense>
         </Container>
     </div>
   )
