73a1c9676d7be2278e2c28641d1d6ab98cd2cda1
Mateusz Bizoń <mat-biz@wp.pl>
2025-11-02 10:29:08 +0100

    changed contact section

--- src/components/sections/ContactSection.tsx
+++ src/components/sections/ContactSection.tsx
@@ -1,26 +1,28 @@
 import React from 'react'
 import { Button } from '../ui/button'
 import Link from 'next/link'
-import { Mail, Phone } from 'lucide-react'
+import { Phone } from 'lucide-react'
 import Container from '../ui/container'
 import { PHONE_NUMBER } from '@/constants'
 
 function ContactSection() {
   return (
-    <section className='py-section px-5 '>
-        <Container className='rounded-4xl p-10 radial-background space-y-10 border'>
-            <h2 className='heading2 text-center heading-margin-bottom'>
-                Rozpocznijmy naukę. Gotowi?
-            </h2>
-            <p className='bigger-text text-center max-w-2xl mx-auto'>
-                Napisz do mnie wiadomość lub zadzwoń, aby umówić się na <strong>pierwszą darmową lekcję</strong> lub dowiedzieć się więcej o ofercie.
-            </p>
-            <div className='flex justify-center'>
-                <Button size={"lg"} asChild>
-                    <Link href={`tel:${PHONE_NUMBER}`}>
-                        <Phone /> {PHONE_NUMBER}
-                    </Link>
-                </Button>
+    <section className='py-section'>
+        <Container>
+            <div className='rounded-4xl px-5 py-10 md:p-10 radial-background space-y-10 border'>
+                <h2 className='heading2 text-center heading-margin-bottom'>
+                    Rozpocznijmy naukę. Gotowi?
+                </h2>
+                <p className='bigger-text text-center max-w-2xl mx-auto'>
+                    Napisz do mnie wiadomość lub zadzwoń, aby umówić się na <strong>pierwszą darmową lekcję</strong> lub dowiedzieć się więcej o ofercie.
+                </p>
+                <div className='flex justify-center'>
+                    <Button size={"lg"} asChild>
+                        <Link href={`tel:${PHONE_NUMBER}`}>
+                            <Phone /> {PHONE_NUMBER}
+                        </Link>
+                    </Button>
+                </div>
             </div>
         </Container>
     </section>
