44866c1f6ce533d573813630b73dce28b78bfeef
Mateusz Bizoń <mat-biz@wp.pl>
2025-11-17 16:27:45 +0100

    added revalidate all blogs for 60 seconds

--- src/components/blog/AllBlogs.tsx
+++ src/components/blog/AllBlogs.tsx
@@ -6,7 +6,9 @@ import BlogCard from '../cards/BlogCard'
 async function AllBlogs() {
     const blogs = await client.withConfig({
         useCdn: false
-    }).fetch(GET_ALL_BLOGS_QUERY)
+    }).fetch(GET_ALL_BLOGS_QUERY, {}, {
+        next: { revalidate: 60 }
+    })
 
   return (
     <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-5 md:gap-10'>
